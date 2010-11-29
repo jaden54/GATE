7750de05a097730c7f936b1310d5f2c7c31cd63d
Vincent <Vincent.Pesenti@telecom-sudparis.eu>
2010-11-29 18:57:00 +0100

    intégration à la mode vêtement des fichiers php
     On branch master
     Changes to be committed:
    	modified:   Gate/QCM/etp1.php
    	modified:   Gate/QCM/show.php
    	modified:   Gate/QCM/test.php

--- Gate/QCM/etp1.php
+++ Gate/QCM/etp1.php
@@ -1,18 +1,62 @@
 <?php
+session_start();
+
+if ( !isset($_SESSION['user']) ) {
+header('Location : nolog.php');
+exit();
+}
+
+include("sqlconnect.php");
 
 $i = 1;
 
-echo '<center><br><br><h2><b><font color="green"> Etape 1 </font></b></h2></center><br>';
-echo '<form method="post" action="etp2.php" name="etp1"><center>';
+$toto = $_SESSION['user'];
+$time = time();
+$toto ="$toto$time";
+if ( isset($_Get['cd']) AND isset($_POST['loc']) ) {
 
-echo '<b><font color="navy">Question '.$i.'</font></b>
-<br><input value="Tappez ici le texte de la question '.$i.'" size="100" name="qtext"><br>
-<b><font color="navy">Numéro de la question  :</font></b><br> <input value="code" size="20" name="qnum"><br>
-<b><font color="navy">Nombre de points  :</font></b><br> <input value="code" size="20" name="pt"><br>
-<b><font color="navy">Code QCM  :</font></b><br> <input value="code" size="20" name="code"><br>
-<b><font color="navy">Nombre de Choix  :</font></b><br> <input value="nombre" size="20" name="chnum"><br>';
+	if($_POST['loc'] == "add") {
+		$toto = $GET['cd'];
+		connect();
+		$req = mysql_query("SELECT COUNT(*) AS qn FROM qcm_elm WHERE code='$toto' ");
+		$tab = mysql_fetch_array($req);
+		$i = $tab['qn'];
+		mysql_close();
+		$i++;
+	}
+}
 
-echo '<br><br><br><input type="submit" value="Etape Suivante" /><br>
-<br><br></center></form>';
+disconnect();
 
+//$code=$toto;
+
+$code=1;
+
+?>
+
+
+
+
+<br>
+<div id="titre"> Ajouter un QCM </div>
+<br>
+<h2> Etape 1 </h2>
+<form method="post" action="etp2.php" name="etp1"><center>
+
+<table style="border-collapse:collapse;border:0px solid #FFFFFF">
+  <tr><td align="right">Question : </td><td><input value="" size="25" name="qtext"></td></tr>
+   <tr><td align="right">Numéro de la question  : </td><td> <input size="25" name="qnum"></td></tr>
+  <tr><td align="right">Nombre de points  : </td><td> <input size="25" name="pt"></td></tr>
+  <tr><td align="right">Nombre de Choix  : </td><td> <input size="25" name="chnum"></td></tr>
+  <tr><td><input type="hidden" value="<?echo $toto; ?>" ></td><td>&nbsp;</td></tr>
+  <tr><td colspan="2" align="center"><input type="submit" value="Etape Suivante" /></td></tr>
+</table>
+
+<br><br></center></form>
+
+  <div id="titre">Qcm actuel :  </div>
+
+<?
+
+  include("show.php");
 ?>
--- Gate/QCM/show.php
+++ Gate/QCM/show.php
@@ -1,7 +1,8 @@
 <?php
-include("sqlconnect.php");
+  //include("sqlconnect.php");
 
-$code = $_GET['id'];
+  //$code = $_GET['id'];
+if (!isset($code)) { $code = $_GET['id']; } //
 $q = array();
 $i = 0;
 
@@ -12,7 +13,6 @@ $i++;
 $q["$i"] = $don['cont'];
 }
 disconnect();
-
 echo '<form method="post" action="vqcm.php" name="show">';
 
 for ($j=1;$j<=$i;$j++) {
@@ -22,7 +22,7 @@ connect();
 $req1 = mysql_query(" SELECT * FROM qcm_elemt WHERE code=$code AND qest=$j ORDER BY num ASC ");
 while ( $done = mysql_fetch_array($req1) ) {
 		if( $done['num'] > 0 ) {
-		echo '<br><b><font color="navy">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="'.$j.'c'.$h.'" value="'.$done['pt'].'" type="checkbox"> &nbsp; '.$done['cont'].' .</font></b>';
+		echo '<br><b><font color="navy">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="'.$j.'c'.$h.'" value="'.$done['pt'].'" type="checkbox"> &nbsp; '.$done['cont'].'</font></b>';
 		}
 		$h++;
 }
